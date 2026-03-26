f4760ee0d17b60d5a086994009da0b33990a0ce4
ShivankShuklaa <shivank@niua.org>
2026-03-26 19:48:10 +0530

    Node 20 Stable,  removed all resolutions

--- frontend/upyog-ui/web/micro-ui-internals/example/src/setupProxy.js
+++ frontend/upyog-ui/web/micro-ui-internals/example/src/setupProxy.js
@@ -3,11 +3,11 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const createProxy = createProxyMiddleware({
   //target: process.env.REACT_APP_PROXY_API || "https://uat.digit.org",
   // target: process.env.REACT_APP_PROXY_API || "https://qa.digit.org",
-  target: process.env.REACT_APP_PROXY_API || "https://qa.digit.org",
+  target: process.env.REACT_APP_PROXY_API || "https://niuatt.niua.in",
   changeOrigin: true,
 });
 const assetsProxy = createProxyMiddleware({
-  target: process.env.REACT_APP_PROXY_ASSETS || "https://qa.digit.org",
+  target: process.env.REACT_APP_PROXY_ASSETS || "https://niuatt.niua.in",
   changeOrigin: true,
 });
 module.exports = function (app) {
