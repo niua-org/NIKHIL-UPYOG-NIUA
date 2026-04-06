d57b8059d1d9e5d99a1fae4755cff4d13e42ef05
Nikhil5922 <nikhilkumar7800@gmail.com>
2026-04-06 10:38:25 +0530

    enable sourcemaps in Vite build configuration

--- frontend/sv-ui/web/vite.config.js
+++ frontend/sv-ui/web/vite.config.js
@@ -56,7 +56,7 @@ export default defineConfig(({ mode }) => {
     },
 
     build: {
-      sourcemap: false,
+      sourcemap: true,
       outDir: "build",
       commonjsOptions: {
         transformMixedEsModules: true,
